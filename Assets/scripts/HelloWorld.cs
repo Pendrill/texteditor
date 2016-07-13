b9c6de098bc642766a7165d1fae1e2f1befb7333
Varsha Prem <vpicequeen@gmail.com>
2016-07-13 06:43:51 -0400

    test commit

--- Assets/scripts/HelloWorld.cs
+++ Assets/scripts/HelloWorld.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class HelloWorld : MonoBehaviour {
+	public Text mytextobject;
 
 	// Use this for initialization
 	void Start () {
+		Debug.Log ("Hello World");
+		mytextobject.text = "vanakam";
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Debug.Log ("Hello Worlds");
 	}
 }
